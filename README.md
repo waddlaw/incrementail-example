a417e0ed7b8fe52fc087c8078e5b7db3c7674fc0
waddlaw <ingronze@gmail.com>
2018-07-24 19:16:05 +0900

    Add example

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# diffs-example
+# incremental-example
+
+```hs
+alice :: Person
+alice = Person 'a' 10
+
+bob :: Person
+bob = Person 'b' 10
+
+fakeBob :: Person
+fakeBob = Person 'B' 14
+
+example :: IO ()
+example = do
+  print $ diff alice bob
+  print $ diff alice fakeBob
+  print $ diff bob fakeBob
+  print $ diff bob bob
+```
+
+```hs
+*Lib Lib> example
+Just (PersonDiff {diffName = Just 'b', diffAge = Nothing})
+Just (PersonDiff {diffName = Just 'B', diffAge = Just 14})
+Just (PersonDiff {diffName = Just 'B', diffAge = Just 14})
+Just (PersonDiff {diffName = Nothing, diffAge = Nothing})
+```hs
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,41 +1,31 @@
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
 module Lib where
 
 import Data.Incremental
-import Data.Text
 
 data Person = Person
-  { name :: Text
+  { name :: Char
   , age  :: Int
   } deriving (Eq, Show)
 
 data PersonDiff = PersonDiff
-  { diffName :: Maybe Text
+  { diffName :: Maybe Char
   , diffAge  :: Maybe Int
   } deriving (Eq, Show)
 
-instance Incremental Text where
-  type Delta Text = Text
-  diff str1 str2 =
-    if str1 == str2
-    then Nothing
-    else Just ""
-
 instance Incremental Person where
   type Delta Person = PersonDiff
+  -- patch _ _ = ()
   diff (Person name1 age1) (Person name2 age2) = Just $ PersonDiff (diff name1 name2) (diff age1 age2)
 
 alice :: Person
-alice = Person "Alice" 10
+alice = Person 'a' 10
 
 bob :: Person
-bob = Person "Bob" 10
+bob = Person 'b' 10
 
 fakeBob :: Person
-fakeBob = Person "BoB" 14
+fakeBob = Person 'B' 14
 
 example :: IO ()
 example = do
